b112fd24a1a49ad689c5ef120c5d8c7ac36d7472
HatCherry <kpospiech.tg@gmail.com>
2023-08-16 20:32:28 +0200

    Add cable.py

---
+++ cable.py
@@ -0,0 +1,15 @@
+# This function returns all the cylinder's derived parameters given it's cross-section in [mm2]
+
+def cable(size):
+    from math import sqrt, pi
+
+    # Unit conversion to [m2]
+    A = size*1E-06
+    # Derived cable data
+    radius = sqrt(A/pi)
+    diameter = radius*2
+    char_length_theory = diameter
+    char_length_coeff = 0.8
+    char_length = char_length_theory*char_length_coeff
+
+    return A, radius, diameter, char_length
