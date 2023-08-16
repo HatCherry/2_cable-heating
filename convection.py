b20cdda6b9287e4c37b2946358ecd005fa515618
HatCherry <kpospiech.tg@gmail.com>
2023-08-16 20:34:56 +0200

    Add in-work convection.py

---
+++ convection.py
@@ -0,0 +1,10 @@
+# This function returns the HTC value of specified cylinder's at any given temperature, in defined ambient
+
+def convection(size, Tcable, Tambient):
+    from cable import cable
+
+    # Get cylinder's data
+    A, radius, diameter, char_length = cable(size)
+
+    
+    return
